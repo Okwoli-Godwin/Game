62baa288cd40ebf0d89e8e820d09693b5f384766
Okwoli-Godwin <okwolig60@gmail.com>
2023-06-08 11:30:15 +0100

    update

--- src/Pages/Gamezone.tsx
+++ src/Pages/Gamezone.tsx
@@ -8,14 +8,22 @@ const Gamezone = () => {
      useEffect(() => {
    setTimeout(() => {
       setIsLoading(true)
-    }, 19800)
+    }, 19840)
 
   }, [])
   return (
       <>
           {isLoading ? (
               
-              <div className="w-full h-screen bg-hero flex justify-center items-center"></div>
+              <div className="w-full h-screen bg-hero flex justify-center items-center">
+                  <div className="w-full flex mt-[70px] items-center">
+                      <div className="w-[300px] h-[2px] bg-gray-100"></div>
+                      <div className="w-[600px] h-[110px] bg-[#002F81]"></div>
+                      <div className="w-[300] h-[2px] bg-gray-100"></div>
+                  </div>
+                      
+                  
+              </div>
           ) : 
                   <Videobackground />
           
